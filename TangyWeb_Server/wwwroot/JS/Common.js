6950bc000a6991246772a0a24b08e1aab832a889
ahmad zaid <agzaid00@gmail.com>
2022-08-29 12:07:01 +0200

    adding toastr and sweet alert

---
+++ TangyWeb_Server/wwwroot/JS/Common.js
@@ -0,0 +1,30 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message,'Operation successful', { timeOut: 5000 })
+    }
+    if (type === "warning") {
+        toastr.warning(message,'Operation Failed', { timeOut: 5000 })
+    }
+    if (type === "error") {
+        toastr.error(message,'Operation Failed', { timeOut: 5000 })
+    }
+}
+
+window.ShowSwal = (type, message) => {
+    if (type === "success") {
+        Swal.fire({
+            title: 'Success Notification',
+            text: message,
+            icon: 'success',
+            confirmButtonText: 'Cool'
+        })
+    }
+    if (type === "error") {
+        Swal.fire({
+            title: 'Error Notification',
+            text: message,
+            icon: 'error',
+            confirmButtonText: 'Cool'
+        })
+    }
+}
