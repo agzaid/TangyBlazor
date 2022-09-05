fe90ec4aa518e23bc59bcf264afe0c05c27d2134
ahmad zaid <agzaid00@gmail.com>
2022-09-05 13:06:17 +0200

    added life cycles

---
+++ Tangy_Business/Repository/CategoryRepository.cs
@@ -0,0 +1,75 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tangy_Business.Repository.IRepository;
+using Tangy_DataAccess;
+using Tangy_DataAccess.Data;
+using Tangy_Models.Models;
+
+namespace Tangy_Business.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CategoryRepository(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task< CategoryDto > Create(CategoryDto objDto)
+        {
+            var obj = _mapper.Map<CategoryDto, Category>(objDto);
+            obj.CreatedDate = DateTime.Now; 
+
+            var addedObj = _context.Categories.Add(obj);
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<Category, CategoryDto>(addedObj.Entity);
+        }
+
+        public async Task< int > Delete(int id)
+        {
+            var obj = await _context.Categories.FirstOrDefaultAsync(l => l.Id == id);
+            if (obj != null)
+            {
+                _context.Categories.Remove(obj);
+                return await _context.SaveChangesAsync();
+            }
+            return 0;
+        }
+
+        public async Task<IEnumerable<CategoryDto>> GetAll()
+        {
+            return _mapper.Map<IEnumerable<Category>, IEnumerable<CategoryDto>>(_context.Categories);
+        }
+
+        public async Task<CategoryDto> GetById(int id)
+        {
+            var obj= await _context.Categories.FirstOrDefaultAsync(l => l.Id == id);
+            if (obj!=null)
+            {
+                return _mapper.Map<Category, CategoryDto>(obj);
+            }
+            return new CategoryDto();
+        }
+
+        public async Task<CategoryDto> Update(CategoryDto objDto)
+        {
+            var obj = await _context.Categories.FirstOrDefaultAsync(u => u.Id == objDto.Id);
+            if (obj!=null)
+            {
+                obj.Name = objDto.Name;
+                _context.Categories.Update(obj);
+                _context.SaveChanges();
+                return _mapper.Map<Category, CategoryDto>(obj);
+            }
+            return objDto;
+        }
+    }
+}
---
+++ Tangy_Models/Models/CategoryDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_Models.Models
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Please enter name...")]
+        public string Name { get; set; }
+    }
+}
